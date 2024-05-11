776a1bc67ce68c5e63b18a7d2c755faa308c9ca2
AshwinGour28 <ashwing2884@gmail.com>
2024-05-12 03:04:25 +0530

    tested backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
@@ -17,4 +18,6 @@ app.listen(3000, ()=>{
     console.log('Server is running on port 3000')
 })
 
+app.use(express.json())
 app.use('/user', userRoutes);
+app.use('/auth', authRoutes);
