fe7b6034a3d141ccd23f2d69447a2c44d22261cb
AshwinGour28 <ashwing2884@gmail.com>
2024-06-12 17:30:54 +0530

    add admin functionality to the user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -40,7 +40,7 @@ export const signin = async (req, res, next) => {
         if(!validPassword){
             return next(errorHandler(400, "Email and Password not matched"));
         }
-        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const token = jwt.sign({id: validUser._id, isAdmin: validUser.isAdmin}, process.env.JWT_SECRET);
         const {password: pass, ...rest} = validUser._doc;
 
         res.status(200).cookie('access_token', token, {httpOnly: true}).json(rest);
@@ -56,7 +56,9 @@ export const google = async(req, res, next) => {
         const user = await User.findOne({ email });
     if (user) {
       const token = jwt.sign(
-        { id: user._id},
+        { id: user._id, 
+          isAdmin: user.isAdmin,
+        },
         process.env.JWT_SECRET
       );
       const { password, ...rest } = user._doc;
@@ -81,7 +83,9 @@ export const google = async(req, res, next) => {
             });
             await newUser.save();
             const token = jwt.sign(
-              { id: newUser._id },
+              { id: newUser._id,
+                isAdmin: newUser.isAdmin,
+               },
               process.env.JWT_SECRET
             );
             const { password, ...rest } = newUser._doc;
